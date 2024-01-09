8952ada932d4f8ca8e95d595f020854229228f3b
Rick Roca <rick.roca@delinea.com>
2024-01-09 14:13:45 -0500

    Initial SBOW Commit

---
+++ Scripts/SecretServer/ServiceNow/Remote Password Changer/ServiceNow Heartbeat.ps1
@@ -0,0 +1,129 @@
+
+#Expected Argumnts @("pw","username", "clientId", "clientSecret", "kid", "tenant", "privuseremail", "privateKeyPEM")
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+#region Set Paramaters and Vaeiables
+[string]$baseURL = $args[0]
+[string]$tokenUrl = "$baseURL/oauth_token.do"
+[string]$api = "$baseURL/api/now"
+[string]$privUsername = $args[1]
+[string]$privPassword = $args[2]
+[string]$clientId = $args[3]
+[string]$clientSecret = $args[4]
+[string]$username = $args[5]
+[string]$password = $args[6] 
+
+
+#Script Constants
+[string]$scope = "useraccount"
+[string]$LogFile = "$env:Program Files\Thycotic Software Ltd\Distributed Engine\log\ServiceNow-Connector.log"
+  [int32]$LogLevel = 3
+[string]$logApplicationHeader = "ServiceNow Heartbeat"
+#endregion
+
+#region Error Handling Functions
+function Write-Log {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateSet(0,1,2,3)]
+        [Int32]$ErrorLevel,
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [string]$Message
+    )
+    # Evaluate Log Level based on global configuration
+    if ($ErrorLevel -le $LogLevel) {
+        # Format message
+        [string]$Timestamp = Get-Date -Format "yyyy-MM-ddThh:mm:sszzz"
+        switch ($ErrorLevel) {
+            "0" { [string]$MessageLevel = "INF0 " }
+            "1" { [string]$MessageLevel = "WARN " }
+            "2" { [string]$MessageLevel = "ERROR" }
+            "3" { [string]$MessageLevel = "DEBUG" }
+        }
+        # Write Log data
+        $MessageString = "{0}`t| {1}`t| {2}`t| {3}" -f $Timestamp, $MessageLevel,$logApplicationHeader, $Message
+        $MessageString | Out-File -FilePath $LogFile -Encoding utf8 -Append -ErrorAction SilentlyContinue
+        # $Color = @{ 0 = 'Green'; 1 = 'Cyan'; 2 = 'Yellow'; 3 = 'Red'}
+        # Write-Host -ForegroundColor $Color[$ErrorLevel] -Object ( $DateTime + $Message)
+    }
+}
+#endregion Error Handling Functions
+
+#region Get Token
+function Get-BearerToken {
+    param (
+        [Parameter(Mandatory=$true, HelpMessage="Privileged User Name")]
+        [System.String]$privUsername,
+        [Parameter(Mandatory=$true, HelpMessage="Privileged User Password")]
+        [System.String]$privassword,
+        [Parameter(Mandatory=$false, HelpMessage="scope needed")]
+        [System.String]$Scope,
+        [Parameter(Mandatory=$true, HelpMessage="Root Url to hit to get bearer token")]
+        [System.String]$Url,
+        [Parameter(Mandatory=$true, HelpMessage="client_id")]
+        [System.String]$clientId,
+        [Parameter(Mandatory=$true, HelpMessage="client_secret")]
+        [System.String]$clientSecret
+      
+    )
+    Write-Log -ErrorLevel 0 -Message "Obtaining Access Token"
+    $body = @{
+        grant_type    = "password"
+        client_id     = $clientId
+        client_secret = $clientSecret
+        username      = $privUsername
+        password      = $privPassword
+        scope         = $scope
+    }
+    try {
+        $result = Invoke-RestMethod -Method Post -Uri $Url -Body $body
+        $token = $result.access_token
+    }
+    catch {
+        $Err = $_
+        Write-Log -ErrorLevel 0 -Message "Retrieving Access Token Failed"
+        Write-Log -ErrorLevel 2 -Message $Err.Exception
+        throw $Err.Exception 
+    }
+    return $token
+}
+#endregion
+function get-pwd-verification{
+    param (
+        [Parameter(Mandatory=$true, HelpMessage="Username to check PW")]
+        [System.String]$Username,
+        [Parameter(Mandatory=$true, HelpMessage="PW val")]
+        [System.String]$PWD,
+        [Parameter(Mandatory=$true, HelpMessage="header")]
+        [hashtable]$headers
+    )
+    try{
+        $securePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+        $creds = New-Object System.Management.Automation.PSCredential ($Username, $securePassword)
+        $response = Invoke-RestMethod -Uri "$api/table/sys_user?sysparm_limit=1" -Credential $creds   -Method Get
+        if ($response.result.Count -eq 1) {
+            
+            $result = $true
+        }
+
+    }
+    catch {
+        $err = $_
+        Write-Log -ErrorLevel 0 -Message "Verify Password Failed - Bad Username or Password"
+        Write-Log -ErrorLevel 2 -Message $Err.Exception
+        throw $Err.Exception 
+    }
+    return $result
+}
+#
+$token = Get-BearerToken -privUsername $privUsername -privassword $privPassword -Scope $scope -Url $tokenUrl -clientId $clientId -clientSecret $clientSecret
+$headers = @{
+    "Authorization" = "Bearer $token"
+    "Accept" = "application/json"
+    "Content-Type" = "application/json"
+}
+#
+
+ $result = get-pwd-verification -Username $username -PW $password -headers $headers
+ return $result
---
+++ Scripts/SecretServer/ServiceNow/Remote Password Changer/readme.md
@@ -0,0 +1,100 @@
+# ServiceNow Remote Password changer
+
+The steps below show how to Set up and configure a ServiceNow Remote Password Changer, and Delinea Secret Server server. 
+
+If you have not already done, so, please follow the steps in the **Overviw.md Document** found [Here](../Overview.md)
+
+## Create Scripts
+
+### Remote Password Changer Script
+
+- Log in to Secret Server Tenant
+- Navigate to **ADMIN** > **Scripts**
+- Click on **Create Script**
+- Fill out the required fields 
+    - **Name**: ( example ServiceNow Remote Password Changer)
+    - **Description**: (Enter something meaningful to your Orgabization)
+    - **Active** (Checked)
+    - **Script Type**: Powershell
+    - **Category**: Password Changing
+    - **Merge Fields**: Leave Blanck
+    - **Script**: Copy and paste the Script included in the file [ServiceNow Remote Password Changer.ps1](./ServiceNow%20Remote%20Password%20Changer.ps1)
+    - Click Save
+    - This completes the creation of the Rempte Password Script
+
+### Heartbeat Script
+
+- Log in to Secret Server Tenant
+- Navigate to **ADMIN** > **Scripts**
+- Click on **Create Script**
+- Fill out the required fields 
+    - **Name**: ( example ServiceNow Heartbeat)
+    - **Description**: (Enter something meaningful to your Orgabization)
+    - **Active** (Checked)
+    - **Script Type**: Powershell
+    - **Category**: Heartbeat
+    - **Merge Fields**: Leave Blanck
+    - **Script**: Copy and paste the Script included in the file [ServiceNow Heartbeat.ps1](./ServiceNow%20Heartbeat.ps1)
+    - Click Save
+    - This completes the creation of the ServiceNow Heartbeat Script
+
+## Create Password Changer
+
+- Log in to Secret Server Tenant (if not alreday logged in)
+- Navigate to **ADMIN** > **Remote Password Changing**
+- Click on Options (Gropdown List) and select ***Configure Password Changers**
+- Click on Create Password Changer
+- Click on ***Base Password Changer* (Dropdown List) and Select PowerShell Script
+- Rnter a Name (Example - ServiceNow Remote Password Changer )
+- Click Save
+ - Under the **Verify Password Changed Commands** section, Enter the following information:
+   - **PowerShell Script**  (DropdownList) Select PowerShell Script or the Script that was Creted in the [Heartbeat](#heartbeat-script)	Section  
+  - **Script Args**: $tenant-url $[1]$username $[1]$password $[1]$client-id $[1]$client-secret $username $password 
+  - Click	**Save**
+
+- Under the **Password Change Commands** Section, Enter the following information:
+  - **PowerShell Script**  (DropdownList) Select PowerShell Script or the Script that was Creted in the [remote-password-changer-script](#remote-password-changer-script)	Section  
+  - **Script Args**: $tenant-url $[1]$username $[1]$password $[1]$client-id $[1]$client-secret $username $newpassword 
+  - Click	**Save**
+- This completes the creationof the RemotePassword Changer
+
+## Update ServiceNow User template
+
+- Log in to Secret Server Tenant (if not alreday logged in)
+- Navigate to **ADMIN** > **Secret Templates**
+- Find and Select the ServiceNow User Template created in the [Overview.md Document](../Overview.md)
+ - Select the **Mapping** Tab 
+ - In the **Password Changing** section, click edit and fill outhe following
+  - **Enable RPC** Checked
+  - **RPC Max Attempts** 12
+  - **RPC Interval Hours** 8
+  - **Enable Heartbeat** Checked
+  - **Heartbeat Interval Hours** 4
+  - **Password Type to use** Select **ServiceNow Remote Password Changer** or the Password Changer create in the [Create Password Changer Section](#create-password-changer)
+- In the **Password Type Fields** Section, fill out the following
+  - **Domain** tenant-url
+  - **Password** Password
+  - **Username** Username
+- Click Save
+- This conpletes the Update ServiceNow User template section
+
+## Update Remote Password Changer
+
+- Log in to Secret Server Tenant (if not alreday logged in)
+- Navigate to **ADMIN** > **Remote Password Changing**
+- Click on Options (Gropdown List) and select ***Configure Password Changers**
+- Select the ServiceNow Remote Password Changer or the Password Changer created in the [create-password-change](#create-password-changer) section
+- Click **Configure Scan Template at the bottom of the pasge**
+- Click Edit
+- Click the **Scan Template to use** (Dropdown List) Select the ServiceNow User template created in the [overview.md Document](../Overview.md)
+- Mapp the following fields that appear after the selection
+  - **tenant-url** -> Domain
+  - **Username -> username
+  - **Password** -> password
+  - Leave all other fields blank
+- Click Save
+- This conpletes the Update Remote Password Changer section
+
+
+
+
