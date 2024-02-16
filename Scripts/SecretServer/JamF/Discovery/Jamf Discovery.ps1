067c5bbb12d1454baa3971eb3fcfa084cd023ef5
jagger <116566+jagger@users.noreply.github.com>
2024-02-16 13:38:25 -0500

    JAMF QA check

--- Scripts/SecretServer/JamF/Discovery/Jamf Discovery.ps1
+++ Scripts/SecretServer/JamF/Discovery/Jamf Discovery.ps1
@@ -1,18 +1,17 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 #region define variables
-#Define Argument Variables
-
+#define Argument Variables
 [string]$DiscoveryMode = $args[0]
 [string]$baseURL = $args[1]
+[string]$clientId = $args[2]
+[string]$clientSecret = $args[3]
+[string]$adminRole = $args[4]
+[string]$svcAcctGroupId = $args[5]
 [string]$tokenUrl = "$baseURL/api/oauth/token"
 [string]$proapi = "$baseURL/api"
 [string]$classicapi = "$baseURL/JSSResource"
-[string]$clientId = $args[2]
-[string]$clientSecret = $args[3]
-[string]$adminRole= $args[4] #Labeled as PrivilegeSet by Jamf
-[string]$svcAcctGroupId = $args[5]
 
-#Script Constants
+#script Constants
 [string]$LogFile = "$env:ProgramFiles\Thycotic Software Ltd\Distributed Engine\log\Jamf-Discovery.log"
 [int32]$LogLevel = 3
 [string]$logApplicationHeader = "Jamf Discovery"
@@ -23,9 +22,9 @@ function Write-Log {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
-        [ValidateSet(0,1,2,3)]
+        [ValidateSet(0, 1, 2, 3)]
         [Int32]$ErrorLevel,
-        [Parameter(Mandatory,ValueFromPipeline)]
+        [Parameter(Mandatory, ValueFromPipeline)]
         [string]$Message
     )
     # Evaluate Log Level based on global configuration
@@ -39,37 +38,29 @@ function Write-Log {
             "3" { [string]$MessageLevel = "DEBUG" }
         }
         # Write Log data
-        $MessageString = "{0}`t| {1}`t| {2}`t| {3}" -f $Timestamp, $MessageLevel,$logApplicationHeader, $Message
+        $MessageString = "{0}`t| {1}`t| {2}`t| {3}" -f $Timestamp, $MessageLevel, $logApplicationHeader, $Message
         $MessageString | Out-File -FilePath $LogFile -Encoding utf8 -Append -ErrorAction SilentlyContinue
-        # $Color = @{ 0 = 'Green'; 1 = 'Cyan'; 2 = 'Yellow'; 3 = 'Red'}
-        # Write-Host -ForegroundColor $Color[$ErrorLevel] -Object ( $DateTime + $Message)
     }
 }
 #endregion Error Handling Functions
-
-
 #region Get Access Token
-#Get Access Token
 try {
     Write-Log -Errorlevel 0 -Message "Obtaining Access Token"
-    # Prepare body for the token request
+    #prepare body for the token request
     $headers = @{
         "Content-Type" = "application/x-www-form-urlencoded"
-        }
+    }
    
     $body = @{
-                grant_type    = "client_credentials"
-                client_id     = $clientId
-                client_secret = $clientSecret
-            }
-    # Make a POST request to obtain the token
+        grant_type    = "client_credentials"
+        client_id     = $clientId
+        client_secret = $clientSecret
+    }
+    #make a POST request to obtain the token
     $response = Invoke-RestMethod -Uri $tokenUrl -Method POST -Body $body -Headers $headers
-
-    # Extract the access token from the response
+    #extract the access token from the response
     $accessToken = $response.access_token
-    
     Write-Log -Errorlevel 0 -Message "Jamf Access Token Successfuly Obtained "
-
 }
 catch {
     $Err = $_
@@ -77,87 +68,68 @@ catch {
     Write-Log -ErrorLevel 2 -Message $Err.Exception
     throw $Err.Exception
 }
-
 #endregion Get Access Token
-
-
-
-#region Discovery Filtering Functions
-<# The isAdmin and isSvcAcct Functions will not be used in Default mode#>
-function isadmin{
+region Discovery Filtering Functions
+function isadmin {
     param(
-    $adminUsers,
-    $userId
+        $adminUsers,
+        $userId
     )
-
-    try
-        {
-            foreach ($adminUser in $adminUsers)
-            { 
-                if($adminUser.id -eq $userId)
-                {
-                    $isadmin = $true
-                    break
-                } 
-                else
-                {
-                    $isadmin = $false
-                }
-                
-            }
-        }
-    catch   
-        {
-            $Err = $_
-            Write-Log -ErrorLevel 0 -Message "Check if Admin Acct Failed"
-            Write-Log -ErrorLevel 2 -Message $Err.Exception
-            throw $Err.Exception 
-        }
-            
-    $isadmin
-}
-function isSvcAcct{
-param(
-$svcAccts,
-$userId
-)
-        try 
-        {
-        if(!$svcaccts){$isSvcAcct = $false}
-        else{
-            foreach($svcacct in $svcAccts){
-                if($svcAcct.id -eq $userid)
-            {
-                $isSvcAcct = $true
+    try {
+        foreach ($adminUser in $adminUsers) { 
+            if ($adminUser.id -eq $userId) {
+                $isadmin = $true
                 break
             } 
-            else
-            {
-                $isSvcAcct = $false
+            else {
+                $isadmin = $false
             }
-        }}
+        }
+    }
+    catch {
+        $Err = $_
+        Write-Log -ErrorLevel 0 -Message "Check if Admin Acct Failed"
+        Write-Log -ErrorLevel 2 -Message $Err.Exception
+        throw $Err.Exception 
+    }
+    return $isadmin
+}
+function isSvcAcct {
+    param(
+        $svcAccts,
+        $userId
+    )
+    try {
+        if (!$svcaccts) { $isSvcAcct = $false }
+        else {
+            foreach ($svcacct in $svcAccts) {
+                if ($svcAcct.id -eq $userid) {
+                    $isSvcAcct = $true
+                    break
+                } 
+                else {
+                    $isSvcAcct = $false
+                }
             }
-    
-catch 
-    {
+        }
+    }
+    catch {
         $Err = $_
         Write-Log -ErrorLevel 0 -Message "Check if Service Acct Failed"
         Write-Log -ErrorLevel 2 -Message $Err.Exception
         throw $Err.Exception 
     }
-
-return $isSvcAcct
+    return $isSvcAcct
 }
-function isLocal{
+function isLocal {
     param(
-    $localUsers,
-    $userId
+        $localUsers,
+        $userId
     )
-    try
-    {
-      $localuserlist = $localusers.account | Where-Object directory_user -like 'False'
-        foreach($user in $localuserlist){
-            if($user.id.equals($userid) -eq $true){
+    try {
+        $localuserlist = $localusers.account | Where-Object directory_user -Like 'False'
+        foreach ($user in $localuserlist) {
+            if ($user.id.equals($userid) -eq $true) {
                 $islocal = $true
                 break 
             }
@@ -166,39 +138,31 @@ function isLocal{
             }
         }
     }
-    catch 
-    {
+    catch {
         $Err = $_
-        Write-Log -ErrorLevel 0 -Message "Check if Is Local Acct Failed"
+        Write-Log -ErrorLevel 0 -Message "Check if is Local Acct Failed"
         Write-Log -ErrorLevel 2 -Message $Err.Exception
-        throw $Err.Exception     }        
-  return $isLocal
+        throw $Err.Exception
+    }
+    return $isLocal
 }
 #endregion
-
-#region Get All Users
-
- try {
+region Get All Users
+try {
     #Create Headers
     $headers = @{
-    "Authorization" = "Bearer $accessToken"     
-    "Accept" = "application/json, application/json"
-    "Content-Type" = "application/json, application/json"
+        "Authorization" = "Bearer $accessToken"     
+        "Accept"        = "application/json, application/json"
+        "Content-Type"  = "application/json, application/json"
     }
-
     Write-Log -Errorlevel 0 -Message "Obtaining List of Users"    
-    
-    # Get All Active Users
-    # Specify Pro API endpoint uri for Users
+    #get all active users
+    #specify pro API endpoint uri for users
     $uri = "$proapi/user"
-
-    # Specify HTTP method
     $method = "get"
-
     # Send HTTP request
     $users = Invoke-RestMethod -Headers $headers -Method $method -Uri $uri
 }
-
 catch {
     $Err = $_    
     Write-Log -ErrorLevel 0 -Message "Failed to retrieve User List"
@@ -206,178 +170,139 @@ catch {
     throw $Err.Exception
 }
 #endregion
-
 #region get admin users
-# Fetching users associated with the $AdminRoles roles (privilege_set)
-function get-AdminUsers{
+fetching users associated with the $AdminRoles roles (privilege_set)
+function get-AdminUsers {
     try {
         Write-Log -Errorlevel 0 -Message "Retrieving  List of Admin Users"       
-        ##Create Roles Array
+        #create roles array
       
-        If ($adminRole)
-        {
-            ### Create Array of Admin Roles
+        If ($adminRole) {
+            #create array of admin roles
 
             $adminRoleArray = $adminRole.split(",") 
             foreach ($role in $adminRoleArray) {
-                # Users can only have one role (Privilege_Set) at a time. Add respective users from each role to the same array:
-                        $adminUsers += $users | Where-Object PrivilegeSet -EQ $Role
-                    }
-             Write-Log -ErrorLevel 0 -Message "Successfully found $($adminUsers.result.Count) Admin Accounts"    
+                #users can only have one role (Privilege_Set) at a time. Add respective users from each role to the same array:
+                $adminUsers += $users | Where-Object PrivilegeSet -EQ $Role
+            }
+            Write-Log -ErrorLevel 0 -Message "Successfully found $($adminUsers.result.Count) Admin Accounts"    
         }   
     }
-catch {
-    $Err = $_
-    Write-Log -ErrorLevel 0 -Message "Failed to retrieve admin User List"
-    Write-Log -ErrorLevel 2 -Message $Err.Exception
-    throw $Err.Exception  
-    }
-return $adminUsers    
-} 
-
-#endregion Get Admin Users  
-
-#region get Service Accounts
-
-# Fetching Accounts associated with this/these Group/s
-function get-SvcAccounts{
-    try {
-        Write-Log -Errorlevel 0 -Message "Retrieving  List of Service Accounts"       
-        ##Create Roles Array
-        If ($svcAcctGroupId)
-        { 
-            ### Create Array of SvcAccount Roles
-            $svcActGroupIdArray = $svcAcctGroupId.split(",") 
-            foreach ($group in $svcActGroupIdArray) {
-                        $svcAccts += $users | Where-Object groupIds -Contains $group
-                    }
-                Write-Log -ErrorLevel 0 -Message "Successfully found $($svcAccountIds.result.Count) Service Accounts"  
-        }
-    }
     catch {
-        $Err = $_    
-        Write-Log -ErrorLevel 0 -Message "Failed to retrieve Service Accounts List"
+        $Err = $_
+        Write-Log -ErrorLevel 0 -Message "Failed to retrieve admin User List"
         Write-Log -ErrorLevel 2 -Message $Err.Exception
         throw $Err.Exception  
     }
-return $svcAccountIds
-}
+    return $adminUsers
 
-#endregion Get Service Accounts  
+    #endregion Get Admin Users
+    #region get Service Accounts
 
-
-#region Get Local Users
-function get-LocalUsers{
-    try{ 
- #Leverage the Classic API to distinguish between Directory_Users and Local users. 
-
-        $headers = @{
-        "Authorization" = "Bearer $accessToken"     
-        "Accept" = "application/json, application/json"
-        "Content-Type" = "application/json, application/json"
+    #fetching Accounts associated with the group(s)
+    function get-SvcAccounts {
+        try {
+            Write-Log -Errorlevel 0 -Message "Retrieving  List of Service Accounts"       
+            #create roles array
+            If ($svcAcctGroupId) { 
+                #create array of SvcAccount roles
+                $svcActGroupIdArray = $svcAcctGroupId.split(",") 
+                foreach ($group in $svcActGroupIdArray) {
+                    $svcAccts += $users | Where-Object groupIds -Contains $group
+                }
+                Write-Log -ErrorLevel 0 -Message "Successfully found $($svcAccountIds.result.Count) Service Accounts"  
+            }
         }
-        
+        catch {
+            $Err = $_    
+            Write-Log -ErrorLevel 0 -Message "Failed to retrieve Service Accounts List"
+            Write-Log -ErrorLevel 2 -Message $Err.Exception
+            throw $Err.Exception  
+        }
+        return $svcAccountIds
+    }
+    #endregion Get Service Accounts
+    #region Get Local Users
+    function get-LocalUsers {
+        try { 
+            #leverage the classic API to distinguish between Directory_Users and Local users. 
+            $headers = @{
+                "Authorization" = "Bearer $accessToken"     
+                "Accept"        = "application/json, application/json"
+                "Content-Type"  = "application/json, application/json"
+            }
             Write-Log -Errorlevel 0 -Message "Obtaining List of Local Users"    
-            
-            # Specify Classic API endpoint uri for Users (Labeled as Accounts in these endpoints)
+            #specify Classic API endpoint uri for users (Labeled as Accounts in these endpoints)
             $uri = "$classicapi/accounts"
-        
-            # Specify HTTP method
             $method = "get"
-        
-            # Send HTTP request
+
+            #send HTTP request
             $LocalusersIds = Invoke-RestMethod -Headers $headers -Method $method -Uri $uri
-        # Create array for each user's information
-          $localusers = @(foreach($localuser in $localusersIds.accounts.users.id){
-                Invoke-RestMethod -Uri "$uri/userid/$localuser" -Headers $headers})
+            #create array for each user's information
+            $localusers = @(foreach ($localuser in $localusersIds.accounts.users.id) {
+                    Invoke-RestMethod -Uri "$uri/userid/$localuser" -Headers $headers
+                })
         }
-    catch {
+        catch {
             $Err = $_    
             Write-Log -ErrorLevel 0 -Message "Failed to retrieve User List"
             Write-Log -ErrorLevel 2 -Message $Err.Exception
             throw $Err.Exception
         }
-     Write-Log -ErrorLevel 0 -Message "Successfully found $($LocalUsers.result.Count) Local Accounts"       
-return $LocalUsers    
+        Write-Log -ErrorLevel 0 -Message "Successfully found $($LocalUsers.result.Count) Local Accounts"       
+        return $LocalUsers
     }
-#endregion Get Local Users
+    #endregion Get Local Users
 
-#region Main Process
+    #region Main Process
 
-#Region Get Advanced User Data
-<#
-    if Discovery Mode is set to default, only the get-LocalAccounts will be run
-#>
+    #Region Get Advanced User Data
+    #if $discoveryMode Mode is set to default, only the get-LocalAccounts will be run
+    if ($DiscoveryMode = "Advanced") {
+        $adminUsers = get-AdminUsers  
+        $svcAccountIds = get-SvcAccounts
+    }
+    $LocalUsers = get-LocalUsers
+    #endregion
 
-if($DiscoveryMode = "Advanced"){
-
-  $adminUsers =  get-AdminUsers  
-  $svcAccountIds = get-SvcAccounts  
-
-}
-$LocalUsers = get-LocalUsers
-#endregion
-
-#define Output Array
-$foundAccounts = @()
-
-Try {
-    #Process Users
-    Write-Log -Errorlevel 0 -Message "Filtering Discovered Users"  
-    if($DiscoveryMode -eq "Default")
-    {
-        foreach ($user in $users)
-        {
-            $userId = $user.id
-            
-            ###check is Local
-          $isLocal = isLocal -localUsers $LocalUsers -userId $userId
-      
-                if($isLocal -eq $true)
-                {   
-                
+    #define Output Array
+    $foundAccounts = @()
+    Try {
+        #Process Users
+        Write-Log -Errorlevel 0 -Message "Filtering Discovered Users"  
+        if ($DiscoveryMode -eq "Default") {
+            foreach ($user in $users) {
+                $userId = $user.id
+                #check is Local
+                $isLocal = isLocal -localUsers $LocalUsers -userId $userId
+                if ($isLocal -eq $true) {   
                     $Username = $user.username
-
                     $object = New-Object -TypeName PSObject
                     $object | Add-Member -MemberType NoteProperty -Name tenanturl -Value $baseURL
                     $object | Add-Member -MemberType NoteProperty -Name username -Value $username
-                    
                     $foundAccounts += $object
-                    
                 }
             }
         }
-    else{
-        foreach ($user in $users)
-        { 
-            ### check if is admin
-            if($adminRole) 
-            {
-                $isadmin = isadmin -adminUsers $adminUsers -userId $user.id
-            }
-            else 
-            {
-                $isadmin = "N\A"
-            }
-
-            #Check Service Account
-            if ($svcAcctGroupId)
-                {
+        else {
+            foreach ($user in $users) { 
+                #check if is admin
+                if ($adminRole) {
+                    $isadmin = isadmin -adminUsers $adminUsers -userId $user.id
+                }
+                else {
+                    $isadmin = "N\A"
+                }
+                #check service account
+                if ($svcAcctGroupId) {
                     $isServiceAccount = isSvcAcct -svcAccts $svcAccountIds -userId $user.Id
                 }   
-                else   
-                {
+                else {
                     $isServiceAccount = $false
                 }                  
-
-            #check is Local
-
-                    $isLocal = isLocal -localUsers $LocalUsers -userId $user.Id
-                
-         
-            if(($isAdmin -eq $true -or $isServiceAccount -eq $true ) -and $isLocal -eq $true )
-                {   
-                
+                #check is Local
+                $isLocal = isLocal -localUsers $LocalUsers -userId $user.Id
+                if (($isAdmin -eq $true -or $isServiceAccount -eq $true ) -and $isLocal -eq $true ) {   
                     $Username = $user.username
                     $object = New-Object -TypeName PSObject
                     $object | Add-Member -MemberType NoteProperty -Name tenant-url -Value $baseURL
@@ -387,20 +312,16 @@ Try {
                     $object | Add-Member -MemberType NoteProperty -Name Local-Account -Value $isLocal
                     
                     $foundAccounts += $object
-                    
-                    
-
                 }
             }
         }
-       
-}
-catch {
-    $Err = $_
-    Write-Log -ErrorLevel 0 -Message "Account Discovery-Filtering failed"
-    Write-Log -ErrorLevel 2 -Message $Err.Exception
-    throw $Err.Exception 
-}
-#endregion Main Process
-Write-Log -ErrorLevel 0 -Message "Successfully Filtered $($foundAccounts.Count) total users."
-return $foundAccounts
+    }
+    catch {
+        $Err = $_
+        Write-Log -ErrorLevel 0 -Message "Account Discovery-Filtering failed"
+        Write-Log -ErrorLevel 2 -Message $Err.Exception
+        throw $Err.Exception
+    }
+    #endregion Main Process
+    Write-Log -ErrorLevel 0 -Message "Successfully Filtered $($foundAccounts.Count) total users."
+    return $foundAccounts
--- Scripts/SecretServer/JamF/Instructions.md
+++ Scripts/SecretServer/JamF/Instructions.md
@@ -1,8 +1,8 @@
 # Jamf Connector Overview
 
-This connectore provides the following functions  
+This connector provides the following functions  
 
-- Discovery of Jamf User Accounts in a given Workspace
+- Discovery of Jamf user accounts in a given Workspace
 - Remote Password Changing Jamf users
 - Heartbeats to verify that user credentials are still valid
 
@@ -24,68 +24,69 @@ This connector utilizes an OAuth 2.0 application in Jamf using the client creden
 - Create an API Role and Client in Jamf Pro:
   - Create an API Role, API Client, and generate a Client Secret. This creates an OAuth source for authorization that the client needs to access the restricted resources on the instance. Reference KB can be found [here](https://learn.jamf.com/bundle/jamf-pro-documentation-current/page/API_Roles_and_Clients.html). 
 - Grant the created API role privileges for discovery and user management:
-  - Read Accounts, Update Accounts
+  - *Read Accounts*, *Update Accounts*
 
-- *** For more information, click here: [Client Credentials](https://developer.jamf.com/jamf-pro/docs/client-credentials), [Pro API Privilege Requirements](https://developer.jamf.com/jamf-pro/docs/privileges-and-deprecations), [Classic API Privilege Requirements](https://developer.jamf.com/jamf-pro/docs/classic-api-minimum-required-privileges-and-endpoint-mapping).
+### For more information refer to the manufacturer documentation
+  - [Client Credentials](https://developer.jamf.com/jamf-pro/docs/client-credentials)
+  - [Pro API Privilege Requirements](https://developer.jamf.com/jamf-pro/docs/privileges-and-deprecations)
+  - [Classic API Privilege Requirements](https://developer.jamf.com/jamf-pro/docs/classic-api-minimum-required-privileges-and-endpoint-mapping).
 
-- Document the following values as they will be needed in the upcoming sections
+- Securely document the following values as they will be needed in the upcoming sections
   - clientId, clientSecret
 
 ## Creating secret template for Jamf Accounts 
-
-### Jamf User Account Template
-
-The following steps are required to create the Secret Template for Jamf Users:
-
-- Log in to the Delinea Secret Server (If you have not already done so)
-- Navigate to Admin / Secret Templates
-- Click on Create / Import Template
-- Click on Import.
-- Copy and Paste the XML in the [Jamf User Template.xml File](./Templates/Jamf%20User%20Account.xml)
-- Click on Save
-- This completes the creation of the User Account template
 
 ### Jamf Client Credentials Template
 
 The following steps are required to create the Secret Template for Jamf Discovery Account:
 
-- Log in to the Delinea Secret Server (If you have not already done so)
-- Navigate to Admin / Secret Templates
-- Click on Create / Import Template
-- Click on Import.
-- Copy and Paste the XML in the [Jamf Client Credentials Template.xml File](./Templates/Jamf%20Client%20Credentials.xml)
-- Click on Save
-- This completes the creation of the User Account template
+- Log in to the Delinea Secret Server 
+- Navigate to ***Administration*** -> ***Secret Templates***
+- Click on ***Create / Import Template***
+- Click on ***Import***
+- Copy and Paste the XML in the [Jamf Client Credentials Template](./Templates/Jamf%20Client%20Credentials.xml)
+- Click on ***Save***
 
+### Jamf User Account Template
+
+The following steps are required to create the Secret Template for Jamf Users:
+
+- Log in to the Delinea Secret Server 
+- Navigate to ***Administration*** -> ***Secret Templates***
+- Click on ***Create / Import Template***
+- Click on ***Import***
+- Copy and Paste the XML in the [Jamf User Template](./Templates/Jamf%20User%20Account.xml)
+- Click on ***Save***
 
 ## Create secret in Secret Server for the Jamf Client Credentials Account
  
-- Log in to the Delinea Secret Server (If you have not already done so)
-- Navigate to Secrets
-- Click on Create Secret
-- Select the template created in the earlier step [Above](#Jamf-discovery-account-template).
+- Log in to the Delinea Secret Server 
+- Navigate to ***Secrets***
+- Click on ***Create Secret***
+- Select the template created in the [earlier step](#Jamf-discovery-account-template).
 - Fill out the required fields with the information from the application registration
-    - Secret Name (for example Jamf API Account )
-    - tenant-url (Jamf base workspace url with no trailing slash, example ```https:\\yourserver.jamfcloud.com``` )
-    - The following field values are as created in the [Create an OAuth Application Registry](#create-an-oauth-application-registry) Section
-    - Client-id
-    - client-secret
-    - admin-roles
-    - Service-Account-Group-Ids
-  - Click Create Secret
-  - This completes the creation of a secret in Secret Server for the Jamf Discovery Account
+    - ***Secret Name:*** Jamf API Account or other descriptive name
+    - ***tenant-url:*** Jamf base workspace url with no trailing slash, example ```https:\\yourserver.jamfcloud.com```
+  - The following field values are as created in the [Create an OAuth Application Registry](#create-an-oauth-application-registry) Section
+    - ***Client-id:***
+    - ***client-secret:***
+    - ***admin-roles:***
+    - ***Service-Account-Group-Ids:***
+  - Click ***Create Secret***
 
 ### Admin Roles and Service Account Group Ids
-- The **admin-roles** field will contain a comma-separated list of roles you designate as **Adminstrators**. For more information on Jamf designated application Roles, click [here](https://learn.jamf.com/bundle/jamf-pro-documentation-current/page/Jamf_Pro_User_Accounts_and_Groups.html).
-  - Example: ```ADMINISTRATOR,CUSTOM```
-- The **Service-Account-Group-Ids** field will contain a comma-separated list of GroupIds you designate as **Service Accounts**. This assumes you have allocated and assigned groups specifically for demarking service accounts. 
+- The **admin-roles** field will contain a comma-separated list of roles you designate as **adminstrators**. 
+- Example: ```ADMINISTRATOR,CUSTOM```
+  >[!INFO]
+  >[For more information on Jamf designated application Roles](https://learn.jamf.com/bundle/jamf-pro-documentation-current/page/Jamf_Pro_User_Accounts_and_Groups.html).
+  
+- The **Service-Account-Group-Ids** field will contain a comma-separated list of GroupIds you designate as **service accounts**. This assumes you have allocated and assigned groups specifically for service accounts. 
   Example: ```5,17,23```
 > [!IMPORTANT]
-> Reference Service Account Group IDs and not the group names.
-
+> Reference Service Account **Group IDs** and not the group names.
 
 ## Next Steps
 
-Once the tasks above are completed you can now proceed to create a [Discovery Scanner](./Discovery/readme.md) 
+Once the tasks above are completed you can now proceed to create the [password changer](./RemotePasswordChanger/readme.md) and then the [discovery scanner](./Discovery/readme.md) 
 
 
