bea9fad9b7635c17412bc79b686afe931d3e0f14
Rob Jagger <116566+jagger@users.noreply.github.com>
2025-10-07 10:14:48 -0400

    Update Secrets_with_leading_trailing_spaces.sql

--- Reports/SecretServer/PlatformMigration/Secrets_with_leading_trailing_spaces.sql
+++ Reports/SecretServer/PlatformMigration/Secrets_with_leading_trailing_spaces.sql
@@ -1,4 +1,4 @@
 select '[' + SecretName+ ']' as [Secret Name in Brackets],s.secretid,s.secretName,f.folderpath
 from tbsecret s
 LEFT JOIN tbFolder f on s.FolderID = f.FolderID
-Where secretname like ' %' or  secretname like '% '
+Where secretname like ' %' or  secretname like '% ' and active = 1 
