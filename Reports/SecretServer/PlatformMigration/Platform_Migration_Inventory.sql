ede775a05fd70f6245f59419274170a6610e8956
Conagher <23546026+ConagherL@users.noreply.github.com>
2026-01-22 11:18:41 -0500

    Update Platform_Migration_Inventory.sql
    Roll back of AD App account check.

--- Reports/SecretServer/PlatformMigration/Platform_Migration_Inventory.sql
+++ Reports/SecretServer/PlatformMigration/Platform_Migration_Inventory.sql
@@ -13,6 +13,7 @@ SELECT '--> Platform Adoption Ready' AS [Item],
 	CASE
 		WHEN EXISTS (SELECT * FROM tbDomain WHERE DomainTypeId = 2 AND Active = 1) THEN 'No' /*OpenLDAP Domains */
 		WHEN (SELECT COUNT(*) FROM tbTeam WHERE Active = 1) > 0 THEN 'No' /* Teams usage*/
+	    WHEN (SELECT COUNT(*) FROM tbUser WHERE Enabled = 1 AND IsApplicationAccount = 1 AND DomainId IS NOT NULL) > 0 THEN 'No'  /*AD Domain Accounts*/
 		WHEN (SELECT COUNT(*) FROM tbEventSubscription) > 0 THEN 'Possible'
 		WHEN (SELECT COUNT(*) FROM tbEventPipelinePolicy) > 0 THEN 'Possible'
 		WHEN (SELECT COUNT(*) FROM tbEventSubscription) = 0 AND (SELECT COUNT(*) FROM tbEventPipelinePolicy) = 0 THEN 'Yes'
