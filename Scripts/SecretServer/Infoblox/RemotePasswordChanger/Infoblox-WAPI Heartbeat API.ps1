dd1cf1b0936920c0fb312591b1329a629a773e38
Rob Jagger <116566+jagger@users.noreply.github.com>
2026-02-25 14:30:47 -0500

    Update Infoblox-WAPI Heartbeat API.ps1
    Typos and input validation updates

--- Scripts/SecretServer/Infoblox/RemotePasswordChanger/Infoblox-WAPI Heartbeat API.ps1
+++ Scripts/SecretServer/Infoblox/RemotePasswordChanger/Infoblox-WAPI Heartbeat API.ps1
@@ -3,7 +3,7 @@
 #Define Argument Variables
 try {
 
-    [string]$baseURL = $args[0] 
+    [uri]$baseURL = $args[0] 
     [string]$Username = $args[1]
     [string]$Password = $args[2]
 }
@@ -17,11 +17,10 @@ catch {
 
 #region Script Constants
 [string]$LogFile = "$env:ProgramFiles\Thycotic Software Ltd\Distributed Engine\log\Infoblox-Heartbeat.log"
-[string]$LogFile = "c:\temp\Infoblox-Heartbeat.log"
 [int32]$LogLevel = 3
 [string]$logApplicationHeader = "Infoblox Heartbeat"
 [string]$authTokenType = "Basic"
-[string]$apiUrl = $baseURL + "/wapi/v2.7"
+[string]$apiUrl = $baseURL.AbsoluteUri + "wapi/v2.7"
 #endregion
 
 # This section allows the Invoke-RESTMethod to ignore Self-Signe Certficates in PowerShell V5
@@ -71,8 +70,8 @@ function Write-Log {
         # Write Log data
         $MessageString = "{0}`t| {1}`t| {2}`t| {3}" -f $Timestamp, $MessageLevel,$logApplicationHeader, $Message
         $MessageString | Out-File -FilePath $LogFile -Encoding utf8 -Append -ErrorAction SilentlyContinue
-        $Color = @{ 0 = 'Green'; 1 = 'Cyan'; 2 = 'Yellow'; 3 = 'Red'}
-        Write-Host -ForegroundColor $Color[$ErrorLevel] -Object ( $DateTime + $Message)
+        # $Color = @{ 0 = 'Green'; 1 = 'Cyan'; 2 = 'Yellow'; 3 = 'Red'}
+        # Write-Host -ForegroundColor $Color[$ErrorLevel] -Object ( $DateTime + $Message)
     }
 }
 #endregion Error Handling Functions
@@ -85,23 +84,23 @@ function Write-Log {
         $returnFields = "name,user_type"
 
         $headers = @{
-            "Authorization" = "$authTokenType $global:B64encodeToken"    
+            "Authorization" = "$authTokenType $B64encodeToken"    
         }
 
         # Specify endpoint uri for UserProfile
         $uri = $apiUrl +"/userprofile?_return_fields=$returnFields"
 
-        Write-Log -Errorlevel 0 -Message "Requesting UserProfile form endpoint $uri"    
+        Write-Log -Errorlevel 0 -Message "Requesting UserProfile from endpoint $uri"    
 
         # Specify HTTP method
         $method = "get"
 
         # Send HTTP request
-        $usersObj = Invoke-RestMethod -Method $method -Uri $uri -Headers $headers
+        $usersObj = Invoke-RestMethod -Method $method -Uri $uri -Headers $headers -ErrorAction Stop
         return $usersObj
         } catch {
             $Err = $_    
-            Write-Log -ErrorLevel 0 -Message "Failed to request UserProfile"
+            Write-Log -ErrorLevel 2 -Message "Failed to request UserProfile"
             Write-Log -ErrorLevel 2 -Message $Err.Exception
             throw $Err.Exception
         }
@@ -119,7 +118,7 @@ try {
     #Auth requires a Base64 encoded string using Username and Password
     $nonEncodeStr = $Username + ":" + $Password
     $bytes =  [System.Text.Encoding]::UTF8.GetBytes($nonEncodeStr)
-    $global:B64encodeToken = [Convert]::ToBase64String($bytes)   
+    $B64encodeToken = [Convert]::ToBase64String($bytes)   
 }
 catch {
     $Err = $_   
@@ -139,7 +138,7 @@ try {
     }
 } catch {
     $Err = $_    
-    Write-Log -ErrorLevel 0 -Message "Failed to Authenticate User"
+    Write-Log -ErrorLevel 2 -Message "Failed to Authenticate User"
     Write-Log -ErrorLevel 2 -Message $Err.Exception
     throw $Err.Exception
 }
